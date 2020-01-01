4bfdba29663e2afa8d1243717bd100106346e078
theKingsRaven <s.i.ninja23@gmail.com>
2020-01-01 15:26:53 -0500

    adding more functionality, still WIP

--- R6SDataReader.py
+++ R6SDataReader.py
@@ -1,14 +1,37 @@
 import cv2 as cv
 import pytesseract as pyte
-# TODO: import python library for comparing strings
+import difflib as diff
+import csv
+import xlsxwriter as xlw
+from typing import List
 
-
+# TODO: make globals into locals bc globals are bad
 killX: int = 1041
 killY: int = 190
 boxW:  int = 115
 boxH:  int = 12
 
-def main(filename: str) -> int:
+def get_usernames_from_csv(filename: str) -> List[str]:
+    assert ".csv" in filename or ".txt" in filename
+    userList: List[str] = []
+    with open(filename) as file:
+        usernames = csv.reader(file, delimiter=",")
+        for row in usernames:
+            userList.append(row[0])
+    return userList
+
+def validate_round_num(parsedround: str, maxRounds: int = 3) -> str:
+    roundList: List[str] = []
+    for i in range(1, maxRounds):
+        roundList.append(f"ROUND {i}")
+    return diff.get_close_matches(parsedround, roundList, 1)[0]
+
+def find_closest_username(parsedname: str, usernames: List[str]) -> str:
+    return diff.get_close_matches(parsedname, usernames, 1)[0]
+
+
+def main(videofile: str, userfile: str) -> int:
+    usernames: List[str] = get_usernames_from_csv(userfile)
     # TODO: add excel spreadsheet stuff
     # TODO: add logic to make sure the same kill is only added once
     # vc = cv.VideoCapture(filename)
@@ -28,6 +51,8 @@ def main(filename: str) -> int:
     text = pyte.image_to_string(th)
 
     print(text)
+
+    diff.get_close_matches()
 
 if __name__ == "__main__":
     import argparse
